3f32115570a6b9af21e6d1041b78521c2c4fe2b2
Kun Wang <kwang3@fulgentgenetics.com>
2023-07-03 17:38:39 -0700

    Initial commit

---
+++ NativeMethods.cs
@@ -0,0 +1,276 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LowIntegrityLevelTestApp
+{
+    public class NativeMethods
+    {
+        public const UInt32 STANDARD_RIGHTS_REQUIRED = 0x000F0000;
+        public const UInt32 STANDARD_RIGHTS_READ = 0x00020000;
+        public const UInt32 TOKEN_ASSIGN_PRIMARY = 0x0001;
+        public const UInt32 TOKEN_DUPLICATE = 0x0002;
+        public const UInt32 TOKEN_IMPERSONATE = 0x0004;
+        public const UInt32 TOKEN_QUERY = 0x0008;
+        public const UInt32 TOKEN_QUERY_SOURCE = 0x0010;
+        public const UInt32 TOKEN_ADJUST_PRIVILEGES = 0x0020;
+        public const UInt32 TOKEN_ADJUST_GROUPS = 0x0040;
+        public const UInt32 TOKEN_ADJUST_DEFAULT = 0x0080;
+        public const UInt32 TOKEN_ADJUST_SESSIONID = 0x0100;
+        public const UInt32 TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY);
+        public const UInt32 TOKEN_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY |
+            TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_QUERY_SOURCE |
+            TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_GROUPS | TOKEN_ADJUST_DEFAULT |
+            TOKEN_ADJUST_SESSIONID);
+
+        public static int SE_GROUP_INTEGRITY = 0x00000020;
+
+        public enum TOKEN_INFORMATION_CLASS
+        {
+            TokenUser = 1,
+            TokenGroups,
+            TokenPrivileges,
+            TokenOwner,
+            TokenPrimaryGroup,
+            TokenDefaultDacl,
+            TokenSource,
+            TokenType,
+            TokenImpersonationLevel,
+            TokenStatistics,
+            TokenRestrictedSids,
+            TokenSessionId,
+            TokenGroupsAndPrivileges,
+            TokenSessionReference,
+            TokenSandBoxInert,
+            TokenAuditPolicy,
+            TokenOrigin,
+            TokenElevationType,
+            TokenLinkedToken,
+            TokenElevation,
+            TokenHasRestrictions,
+            TokenAccessInformation,
+            TokenVirtualizationAllowed,
+            TokenVirtualizationEnabled,
+            TokenIntegrityLevel,
+            TokenUIAccess,
+            TokenMandatoryPolicy,
+            TokenLogonSid,
+            TokenIsAppContainer,
+            TokenCapabilities,
+            TokenAppContainerSid,
+            TokenAppContainerNumber,
+            TokenUserClaimAttributes,
+            TokenDeviceClaimAttributes,
+            TokenRestrictedUserClaimAttributes,
+            TokenRestrictedDeviceClaimAttributes,
+            TokenDeviceGroups,
+            TokenRestrictedDeviceGroups,
+            TokenSecurityAttributes,
+            TokenIsRestricted,
+            TokenProcessTrustLevel,
+            TokenPrivateNameSpace,
+            TokenSingletonAttributes,
+            TokenBnoIsolation,
+            TokenChildProcessFlags,
+            TokenIsLessPrivilegedAppContainer,
+            TokenIsSandboxed,
+            TokenIsAppSilo,
+            TokenLoggingInformation,
+            MaxTokenInfoClass
+        }
+
+        public enum TOKEN_TYPE
+        {
+            TokenPrimary = 1,
+            TokenImpersonation
+        }
+
+        public enum SECURITY_IMPERSONATION_LEVEL
+        {
+            SecurityAnonymous,
+            SecurityIdentification,
+            SecurityImpersonation,
+            SecurityDelegation
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SID_AND_ATTRIBUTES
+        {
+
+            public IntPtr Sid;
+            public int Attributes;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TOKEN_MANDATORY_LABEL
+        {
+            public SID_AND_ATTRIBUTES Label;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LocalFree(IntPtr hMem);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetCurrentProcess();
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool OpenProcessToken(IntPtr ProcessHandle, UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool DuplicateTokenEx(
+            IntPtr hExistingToken,
+            uint dwDesiredAccess,
+            IntPtr /*ref SECURITY_ATTRIBUTES*/ lpTokenAttributes,
+            SECURITY_IMPERSONATION_LEVEL ImpersonationLevel,
+            TOKEN_TYPE TokenType,
+            out IntPtr phNewToken);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool ConvertStringSidToSid(
+            string StringSid,
+            out IntPtr ptrSid
+            );
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean SetTokenInformation(
+            IntPtr TokenHandle,
+            TOKEN_INFORMATION_CLASS TokenInformationClass,
+            ref TOKEN_MANDATORY_LABEL TokenInformation,
+            UInt32 TokenInformationLength);
+
+        [DllImport("advapi32.dll")]
+        public static extern uint GetLengthSid(IntPtr pSid);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool CreateProcessAsUser(
+            IntPtr hToken,
+            string lpApplicationName,
+            string lpCommandLine,
+            IntPtr/*ref SECURITY_ATTRIBUTES*/ lpProcessAttributes,
+            IntPtr/*ref SECURITY_ATTRIBUTES*/ lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            IntPtr/*string*/ lpCurrentDirectory,
+            ref STARTUPINFO lpStartupInfo,
+            ref PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hHandle);
+
+        public static void CreateLowProcess(string fileName)
+        {
+            IntPtr hToken = IntPtr.Zero;
+            IntPtr hNewToken = IntPtr.Zero;
+            IntPtr pIntegritySid = IntPtr.Zero;
+
+            while (true)
+            {
+                bool fRet = OpenProcessToken(GetCurrentProcess(), TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_QUERY | TOKEN_ASSIGN_PRIMARY, out hToken);
+                if (!fRet)
+                {
+                    break;
+                }
+
+                fRet = DuplicateTokenEx(
+                    hToken,
+                    0,
+                    IntPtr.Zero,
+                    SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation,
+                    TOKEN_TYPE.TokenPrimary,
+                    out hNewToken);
+                if (!fRet)
+                {
+                    break;
+                }
+
+                string integSidStr = "S-1-16-4096";
+                fRet = ConvertStringSidToSid(integSidStr, out pIntegritySid);
+                if (!fRet)
+                {
+                    break;
+                }
+
+                TOKEN_MANDATORY_LABEL TIL = new TOKEN_MANDATORY_LABEL();
+                TIL.Label.Attributes = SE_GROUP_INTEGRITY;
+                TIL.Label.Sid = pIntegritySid;
+
+                fRet = SetTokenInformation(hNewToken,
+                    TOKEN_INFORMATION_CLASS.TokenIntegrityLevel,
+                    ref TIL,
+                    (uint)Marshal.SizeOf(TIL) + GetLengthSid(pIntegritySid));
+                if (!fRet)
+                {
+                    break;
+                }
+
+                PROCESS_INFORMATION ProcInfo = new PROCESS_INFORMATION();
+                STARTUPINFO StartupInfo = new STARTUPINFO();
+                StartupInfo.cb = Marshal.SizeOf(StartupInfo);
+
+                fRet = CreateProcessAsUser(
+                     hNewToken,
+                     null,
+                     fileName,
+                     IntPtr.Zero,
+                     IntPtr.Zero,
+                     false,
+                     0,
+                     IntPtr.Zero,
+                     IntPtr.Zero,
+                     ref StartupInfo,
+                     ref ProcInfo);
+                break;
+            }
+
+
+            if (hNewToken != IntPtr.Zero)
+            {
+                CloseHandle(hNewToken);
+            }
+
+            if (hToken != IntPtr.Zero)
+            {
+                CloseHandle(hToken);
+            }
+
+            if (pIntegritySid != IntPtr.Zero)
+            {
+                LocalFree(pIntegritySid);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LowIntegrityLevelTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NativeMethods.CreateLowProcess(@"C:\Storage\Code-p\GrpcGreeter\GrpcGreeterClient\bin\Debug\net6.0\GrpcGreeterClient.exe");
+        }
+    }
+}
